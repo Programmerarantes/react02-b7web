3fc1225f7fe5c2a1f94b49f8ef81b9d006ae6602
Matheus <125316650+Programmerarantes@users.noreply.github.com>
2024-03-06 22:32:44 -0300

    updating-objects-in-states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,44 @@
 "use client"
 
+import { Person } from "@/types/Person"
 import { useState } from "react"
 
 const Page = () => {
+  const [fullName, setFullName] = useState<Person>({ name: 'Matheus', lastName: 'Arantes' })
 
-  const [nameInput, setNameInput] = useState('')
+  const handleClearBtn = () => {
+    setFullName({name: '', lastName: ''})
+  }
+  const handleClearName = () => {
+    setFullName({...fullName, name: ''})
+  }
+  const handleClearLastName = () => {
+    setFullName({...fullName, lastName: ''})
+  }
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center">
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-3xl">
+      <h1 className="mb-4">Updating Objects in States - React</h1>
       <input 
         type="text" 
-        className="border-black p-3 text-xl text-black rounded-md"
-        placeholder="Digite seu nome"
-        value={nameInput}
-        onChange={e => setNameInput(e.target.value)}
+        placeholder="Nome"
+        className="border border-black p-3 text-2xl text-black rounded-md mb-3"
+        value={fullName.name}
+        onChange={e => setFullName({...fullName, name: e.target.value})}
       />
-      <p>Seu nome é: {nameInput}</p>  
+      <input 
+        type="text" 
+        placeholder="Sobrenome"
+        className="border border-black p-3 text-2xl text-black rounded-md mb-3"
+        value={fullName.lastName}
+        onChange={e => setFullName({...fullName, lastName: e.target.value})}
+      />
+      <p>Meu nome completo é: </p>
+      <p>{fullName.name} {fullName.lastName}</p>
+
+      <button onClick={handleClearBtn} className="border bg-white text-black rounded-md p-3 m-2 text-xl">Limpar Tudo</button>
+      <button onClick={handleClearName} className="border bg-white text-black rounded-md p-3 m-2 text-xl">Limpar Nome</button>
+      <button onClick={handleClearLastName} className="border bg-white text-black rounded-md p-3 m-2 text-xl">Limpar Sobrenome</button>
     </div>
   )
 }
